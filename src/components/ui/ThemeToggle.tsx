e4f291af12e8a2a5d99824ca424ffe22596b0081
Luiz Guandalini <luizluizcorinthians@gmail.com>
2026-01-06 23:48:29 -0300

    corrigido bug no tema claro escuro

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -28,7 +28,7 @@ const ThemeToggle: React.FC = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364-6.364l-.707.707M6.343 17.657l-.707.707M17.657 17.657l-.707-.707M6.343 6.343l-.707-.707MD12 8a4 4 0 100 8 4 4 0 000-8z"
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364-6.364l-.707.707M6.343 17.657l-.707.707M17.657 17.657l-.707-.707M6.343 6.343l-.707-.707"
             />
             <circle cx="12" cy="12" r="4" stroke="currentColor" strokeWidth="2" />
           </svg>
